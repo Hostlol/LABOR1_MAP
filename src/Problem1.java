d092d222ec9f0450764faf3a8729083b2640bac9
Hostlol <b_chiorean@yahoo.com>
2023-10-17 00:22:32 +0300

    test

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,5 +1,81 @@
 import java.util.ArrayList;
 public class Problem1 {
-    ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+    public static int[] unreichendeNoten(int[] array) {
+        int[] result = new int[array.length];
+        int i = 0;
+        for (int j : array) {
+            if (j < 40)
+            {
+                result[i++] = j;
+            }
+        }
 
+        int[] unreichendeNoten = new int[i];
+        System.arraycopy(result, 0, unreichendeNoten, 0, i);
+
+        return unreichendeNoten;
+    }
+    public static int durchschnittlicherWert(int[] array) {
+        int[] result = new int[array.length];
+        int i = 0;
+        int sum = 0;
+        for (int j : array) {
+            sum = sum + j;
+        }
+        sum = sum / array.length;
+
+        return sum;
+    }
+    public static int[] gerundeteNoten(int[] array) {
+        int[] result = new int[array.length];
+        int i = 0;
+        for (int j : array) {
+            if ((j % 5 > 2) && (j > 38))
+            {
+                result[i++] = j + 5 - (j%5);
+            }
+            else
+            {
+                result[i++] = j;
+            }
+
+        }
+
+        int[] gerundeteNoten = new int[i];
+        System.arraycopy(result, 0, gerundeteNoten, 0, i);
+
+        return gerundeteNoten;
+    }
+    public static int maxGerundeteNote(int[] array) {
+        int[] result = new int[array.length];
+        int i = 0;
+        for (int j : array) {
+            if ((j % 5 > 2) && (j > 38))
+            {
+                result[i++] = j + 5 - (j%5);
+            }
+            else
+            {
+                result[i++] = j;
+            }
+
+        }
+
+        int[] gerundeteNoten = new int[i];
+        System.arraycopy(result, 0, gerundeteNoten, 0, i);
+        int max = 0;
+        for(int q : gerundeteNoten)
+        {
+            if (max < q)
+            {
+                max = q;
+            }
+
+        }
+
+        return max;
+    }
 }
+
+
+
